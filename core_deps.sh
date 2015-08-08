acaf3bcf8b49c3beda3acd930864656ae21736d9
Ross Engers <rossengers@gmail.com>
2015-08-08 20:04:41 +0200

    Fix name in java rpm file
    * Add more dependencies needed for compiling ruby on rhel

--- core_deps.sh
+++ core_deps.sh
@@ -11,18 +11,19 @@ then
   sudo yum install -y python27
   sudo yum install -y git
   sudo yum install -y vim
+  sudo yum install -y gcc openssl-devel readline-devel zlib-devel
   git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
   git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 
   # Java
   echo "Installing Java 1.7"
   curl -v -j -k -L -H "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u79-b15/jdk-7u79-linux-x64.rpm > jdk-7u79-linux-x64.rpm
-  sudo rpm -Uvh jdk-7u79-linux-i586.rpm
+  sudo rpm -Uvh jdk-7u79-linux-x64.rpm
   rm jdk-7u79-linux-x64.rpm
 
   echo "Installing Java 1.8"
   curl -v -j -k -L -H "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u51-b16/jdk-8u51-linux-x64.rpm > jdk-8u51-linux-x64.rpm
-  sudo rpm -Uvh jdk-8u51-linux-i586.rpm
+  sudo rpm -Uvh jdk-8u51-linux-x64.rpm
   rm jdk-8u51-linux-x64.rpm
 fi
 
