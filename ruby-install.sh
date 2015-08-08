7cffe5a78d8bb197ba1400c76bc9f5ad6dd3bde0
Ross Engers <rossengers@gmail.com>
2015-08-08 18:04:28 +0200

    Fix ruby install to use rbenv install --list

--- ruby-install.sh
+++ ruby-install.sh
@@ -5,10 +5,10 @@ export PATH=$HOME/.rbenv/bin:$PATH
 eval "$(rbenv init -)"
 
 # Requires both rbenv and ruby-build to be installed
-(rbenv --version && ruby-build --version) || exit 1
+(rbenv --version && rbenv install --version) || exit 1
 
 # Get latest stable cruby version
-LATEST_CRUBY_VERSION=`ruby-build --definitions | grep -E "^[0-9\.]+$" | sort | tail -n 1`
+LATEST_CRUBY_VERSION=`rbenv install --list | grep -E "^[0-9\.]+$" | sort | tail -n 1`
 
 # Only install if the version is not present
 (rbenv versions | grep $LATEST_CRUBY_VERSION) && echo "CRuby $LATEST_CRUBY_VERSION already installed!" && exit 0
