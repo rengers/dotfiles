eef9a02a7aa2c2cc183f105186d0295dd8c34210
Ross Engers <rengers@amazon.com>
2019-01-31 13:28:13 -0800

    No more rhel

--- core_deps.sh
+++ core_deps.sh
@@ -2,7 +2,7 @@
 
 source $HOME/dotfiles/platforms.sh
 
-if is_alinux || is_rhel
+if is_alinux
 then
   # Keep-alive: update existing `sudo` time stamp until the script has finished.
   while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
