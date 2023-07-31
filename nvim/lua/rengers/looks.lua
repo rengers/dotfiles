4409a8f91117dab9212f20c4728a086abf28521a
Ross Engers <rengers@amazon.com>
2023-07-31 15:45:25 -0400

    Remove unused functions
    * move looks config to native lua

--- nvim/lua/rengers/looks.lua
+++ nvim/lua/rengers/looks.lua
@@ -1,3 +1,36 @@
 vim.cmd [[colorscheme catppuccin]]
 
+vim.wo.number = true
+vim.opt.laststatus=2 --always display status line
+vim.wo.cursorline = true
+vim.opt.showmode = false
+vim.opt.foldlevel = 99
+
+
+-- Highlight trailing space
+vim.cmd("highlight ExtraWhitespace ctermbg=red guibg=red")
+
+local function set_extra_whitespace_match(pattern)
+  vim.fn.matchadd('ExtraWhitespace', pattern)
+end
+
+local function clear_extra_whitespace_match()
+  vim.fn.clearmatches()
+end
+
+local whitespace_group = vim.api.nvim_create_augroup("ExtraWhitespace", { clear = true })
+
+-- Matching trailing space on different events
+vim.api.nvim_create_autocmd("BufWinEnter", { pattern = "*", callback = function() set_extra_whitespace_match('\\s\\+$') end, group = whitespace_group })
+vim.api.nvim_create_autocmd("InsertEnter", { pattern = "*", callback = function() set_extra_whitespace_match('\\s\\+\\%#@<!$') end, group = whitespace_group })
+vim.api.nvim_create_autocmd("InsertLeave", { pattern = "*", callback = function() set_extra_whitespace_match('\\s\\+$') end, group = whitespace_group })
+vim.api.nvim_create_autocmd("BufWinLeave", { pattern = "*", callback = clear_extra_whitespace_match, group = whitespace_group })
+
+
+-- Ported from old config to set 24-bit RGB
+if vim.o.termguicolors then
+  vim.o.t_8f = "\\e[38;2;%lu;%lu;%lum"
+  vim.o.t_8b = "\\e[48;2;%lu;%lu;%lum"
+  vim.o.termguicolors = true
+end
 
--- vim/vimrc/functions.vim
+++ vim/vimrc/functions.vim
@@ -1,13 +1,3 @@
-func! Cwd()
-  let cwd = getcwd()
-  return "e " . cwd
-endfunc
-
-
-func! CurrentFileDir(cmd)
-  return a:cmd . " " . expand("%:p:h") . "/"
-endfunc
-
 
 command! Bclose call <SID>BufcloseCloseIt()
 function! <SID>BufcloseCloseIt()
@@ -29,49 +19,6 @@ function! <SID>BufcloseCloseIt()
    endif
 endfunction
 
-
-function! CurDir()
-    return substitute(getcwd(), "$HOME/", "~/", "g")
-endfunction
-
-
-function! HasPaste()
-    if &paste
-        return 'PASTE MODE  '
-    en
-    return ''
-endfunction
-
-"
-" From an idea by Michael Naumann
-"
-function! CmdLine(str)
-    exe "menu Foo.Bar :" . a:str
-    emenu Foo.Bar
-    unmenu Foo
-endfunction
-
-
-function! VisualSelection(direction) range
-    let l:saved_reg = @"
-    execute "normal! vgvy"
-
-    let l:pattern = escape(@", '\\/.*$^~[]')
-    let l:pattern = substitute(l:pattern, "\n$", "", "")
-
-    if a:direction == 'b'
-        execute "normal ?" . l:pattern . "^M"
-    elseif a:direction == 'gv'
-        call CmdLine("vimgrep " . '/'. l:pattern . '/' . ' **/*.')
-    elseif a:direction == 'replace'
-        call CmdLine("%s" . '/'. l:pattern . '/')
-    elseif a:direction == 'f'
-        execute "normal /" . l:pattern . "^M"
-    endif
-
-    let @/ = l:pattern
-    let @" = l:saved_reg
-endfunction
 
 function! GetBufferList()
   redir =>buflist
