644690806a12c619644e87c25c50be74d20aec13
Ross Engers <rengers@amazon.com>
2023-08-01 17:42:08 -0400

    And keymap for git push

--- nvim/lua/rengers/plugins/fugitive.lua
+++ nvim/lua/rengers/plugins/fugitive.lua
@@ -2,6 +2,7 @@ return {
     "tpope/vim-fugitive",
     config = function()
       vim.api.nvim_set_keymap('n', '<leader>gs', ":Git<cr>", { noremap = true, silent = true });
+      vim.api.nvim_set_keymap('n', '<leader>gp', ":Git push<cr>", { noremap = true, silent = true });
       vim.opt.diffopt:append("vertical")
 
       local fug_group = vim.api.nvim_create_augroup("Fugitive hidden", { clear = true })
