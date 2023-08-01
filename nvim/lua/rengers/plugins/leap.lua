401f086eb53dc9b905fdef6dc7c81fa07a79c4c7
Ross Engers <rengers@amazon.com>
2023-08-01 18:09:01 -0400

    Remove default mappings for leap
    * I want s anv x in visual back
    * mapped leap key to <leader>s etc

--- nvim/lua/rengers/plugins/leap.lua
+++ nvim/lua/rengers/plugins/leap.lua
@@ -2,7 +2,16 @@ return {
   "ggandor/leap.nvim",
 
   config = function()
-    require('leap').add_default_mappings()
+    --require('leap').add_default_mappings()
+    -- mappings
+    local keymap_opts = { buffer = false, silent = true, noremap = true }
+    vim.keymap.set("n", "<leader>s", "<Plug>(leap-forward-to)", keymap_opts)
+    vim.keymap.set("n", "<leader>S", "<Plug>(leap-backward-to)", keymap_opts)
+    vim.keymap.set("v", "<leader>s", "<Plug>(leap-forward-to)", keymap_opts)
+    vim.keymap.set("v", "<leader>S", "<Plug>(leap-backward-to)", keymap_opts)
+    vim.keymap.set("v", "<leader>x", "<Plug>(leap-forward-till)", keymap_opts)
+    vim.keymap.set("v", "<leader>X", "<Plug>(leap-backward-till)", keymap_opts)
+
     -- The below settings make Leap's highlighting a bit closer to what you've been
     -- used to in Lightspeed.
     vim.api.nvim_set_hl(0, 'LeapBackdrop', { link = 'Comment' })
